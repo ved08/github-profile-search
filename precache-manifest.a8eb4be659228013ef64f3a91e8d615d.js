a785439eb1d59abed0d4aef75a6da56f102c6a0e
Vedvardhan Gyanmote <vedvardhan.g@gmail.com>
2020-08-18 15:02:38 +0530

    Updates

---
+++ precache-manifest.a8eb4be659228013ef64f3a91e8d615d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a52bb455d9110ccfcaf69733e6da944",
+    "url": "/github-profile-search/index.html"
+  },
+  {
+    "revision": "969d3c091864c52f3dae",
+    "url": "/github-profile-search/static/css/main.fb6e867c.chunk.css"
+  },
+  {
+    "revision": "c2ecbc2d5284b581fe19",
+    "url": "/github-profile-search/static/js/2.206b14bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-profile-search/static/js/2.206b14bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "969d3c091864c52f3dae",
+    "url": "/github-profile-search/static/js/main.4646971f.chunk.js"
+  },
+  {
+    "revision": "7381e24eff764774b8d3",
+    "url": "/github-profile-search/static/js/runtime-main.b4462f59.js"
+  }
+]);
